dd3f88e7cb98851ab1993217f925d1726b2599b8
Celine <celine@demandmanager.com.au>
2021-03-21 15:20:40 +1100

    exception handler

--- src/main/kotlin/com/celine/imagesteganography/errors/ExceptionHandler.kt
+++ src/main/kotlin/com/celine/imagesteganography/errors/ExceptionHandler.kt
@@ -1,4 +1,17 @@
 package com.celine.imagesteganography.errors
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+
+@ControllerAdvice
 class ExceptionHandler {
+
+    @ExceptionHandler(Exception::class)
+    fun handleException(ex: Exception): ResponseEntity<Any> {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.message?: "Internal Error"))
+    }
 }
